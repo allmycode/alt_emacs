857e6c7b6d2a546d4f676ee489c52576690bc65f
Stanislav Lyakhnovich <stanislav.lyakhnovich@db.com>
2015-03-04 12:43:42 +0300

    Fix for helm ffap

--- init.el
+++ init.el
@@ -43,6 +43,7 @@
   (progn 
     (helm-mode 1)
 
+    (global-set-key (kbd "C-x C-f") 'helm-find-files)
     (global-set-key (kbd "C-x b") 'helm-mini)
     (global-set-key (kbd "M-x") 'helm-M-x)
     (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
@@ -55,7 +56,10 @@
 
     (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
     (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-    (define-key helm-map (kbd "C-z")  'helm-select-action))) ; list actions using C-z
+    (define-key helm-map (kbd "C-z")  'helm-select-action)  ; list actions using C-z
+    
+    (setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
+    ))
 
 ;; DIRED-MODE
 (req-package dired
